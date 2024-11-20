422091a86b5415119a2301b3c4f0011cfa87b73f
kali <kali@kali>
2024-11-20 20:35:57 +0300

    Added branch main

--- build_beckup.sh
+++ build_beckup.sh
@@ -9,7 +9,7 @@ MYSQL_USER=$1
 MYSQL_PASSWORD=$2
 MYSQL_DB=$3
 CONTAINER_NAME="policlinic_db_1"
-BACKUP_FILE="$(pwd)/backup.sql"
+BACKUP_FILE="$(pwd)/policlinic/docker_volumes/backup.sql"
 
 docker exec "$CONTAINER_NAME" mysqldump -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" $MYSQL_DB > "$BACKUP_FILE"
 
