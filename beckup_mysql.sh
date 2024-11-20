061157f42ae771993214552bc7a601d1d979f68e
kali <kali@kali>
2024-11-20 20:23:04 +0300

    Modified files

--- beckup_mysql.sh
+++ beckup_mysql.sh
@@ -10,6 +10,6 @@ MYSQL_USER=$1
 MYSQL_PASSWORD=$2
 MYSQL_DB=$3
 CONTAINER_NAME="policlinic_db_1"
-BACKUP_FILE="./backup.sql"
+BACKUP_FILE="$(pwd)/backup.sql"
 
 docker exec -i "$CONTAINER_NAME" mysql -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" $MYSQL_DB < "$BACKUP_FILE"
